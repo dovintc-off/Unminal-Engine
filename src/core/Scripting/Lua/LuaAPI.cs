b865af99f2139a84fd2c99a27e843cc8588eca14
Dov1ntc <dovintc@gmail.com>
2026-08-20 03:43:09 +0300

    Add global engine state and keyboard/mouse events to Lua scripting

--- src/core/Scripting/Lua/LuaAPI.cs
+++ src/core/Scripting/Lua/LuaAPI.cs
@@ -10,12 +10,13 @@ using Unminal.UI.ButtonObject;
 using Unminal.UI.Factory;
 using Unminal.Core.Scripting.Lua.WraperStates;
 using NLua;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 [SupportedOSPlatform("Windows")]
 public static class LuaAPI {
     public static Camera RegisterApi(Lua lua, Camera camera) {
         lua.NewTable("unminal");
-        lua["unminal.Engine"] = new WraperEngineStates();
+
         lua["unminal.log"] = new Action<string>(msg => 
             Log.Create(Log.LogType.INFO, $"[Lua] {msg}"));
 
@@ -64,8 +65,107 @@ public static class LuaAPI {
             }
         });
 
-        lua["unminal.SetCursorNormal"] = new Action(() => Engine.LightManager?.ClearLights());
-
+        lua["unminal.Engine"] = new WraperEngineStates();
+        lua.NewTable("unminal.keys");
+        // KEYBOARD MOUSE
+        lua["unminal.keys.Unknown"] = (int)Keys.Unknown;
+        lua["unminal.keys.Space"] = (int)Keys.Space;
+        lua["unminal.keys.Apostrophe"] = (int)Keys.Apostrophe;
+        lua["unminal.keys.Comma"] = (int)Keys.Comma;
+        lua["unminal.keys.Minus"] = (int)Keys.Minus;
+        lua["unminal.keys.Period"] = (int)Keys.Period;
+        lua["unminal.keys.Slash"] = (int)Keys.Slash;
+        lua["unminal.keys.D0"] = (int)Keys.D0;
+        lua["unminal.keys.D1"] = (int)Keys.D1;
+        lua["unminal.keys.D2"] = (int)Keys.D2;
+        lua["unminal.keys.D3"] = (int)Keys.D3;
+        lua["unminal.keys.D4"] = (int)Keys.D4;
+        lua["unminal.keys.D5"] = (int)Keys.D5;
+        lua["unminal.keys.D6"] = (int)Keys.D6;
+        lua["unminal.keys.D7"] = (int)Keys.D7;
+        lua["unminal.keys.D8"] = (int)Keys.D8;
+        lua["unminal.keys.D9"] = (int)Keys.D9;
+        lua["unminal.keys.Semicolon"] = (int)Keys.Semicolon;
+        lua["unminal.keys.Equal"] = (int)Keys.Equal;
+        lua["unminal.keys.A"] = (int)Keys.A;
+        lua["unminal.keys.B"] = (int)Keys.B;
+        lua["unminal.keys.C"] = (int)Keys.C;
+        lua["unminal.keys.D"] = (int)Keys.D;
+        lua["unminal.keys.E"] = (int)Keys.E;
+        lua["unminal.keys.F"] = (int)Keys.F;
+        lua["unminal.keys.G"] = (int)Keys.G;
+        lua["unminal.keys.H"] = (int)Keys.H;
+        lua["unminal.keys.I"] = (int)Keys.I;
+        lua["unminal.keys.J"] = (int)Keys.J;
+        lua["unminal.keys.K"] = (int)Keys.K;
+        lua["unminal.keys.L"] = (int)Keys.L;
+        lua["unminal.keys.M"] = (int)Keys.M;
+        lua["unminal.keys.N"] = (int)Keys.N;
+        lua["unminal.keys.O"] = (int)Keys.O;
+        lua["unminal.keys.P"] = (int)Keys.P;
+        lua["unminal.keys.Q"] = (int)Keys.Q;
+        lua["unminal.keys.R"] = (int)Keys.R;
+        lua["unminal.keys.S"] = (int)Keys.S;
+        lua["unminal.keys.T"] = (int)Keys.T;
+        lua["unminal.keys.U"] = (int)Keys.U;
+        lua["unminal.keys.V"] = (int)Keys.V;
+        lua["unminal.keys.W"] = (int)Keys.W;
+        lua["unminal.keys.X"] = (int)Keys.X;
+        lua["unminal.keys.Y"] = (int)Keys.Y;
+        lua["unminal.keys.Z"] = (int)Keys.Z;
+        lua["unminal.keys.LeftBracket"] = (int)Keys.LeftBracket;
+        lua["unminal.keys.Backslash"] = (int)Keys.Backslash;
+        lua["unminal.keys.RightBracket"] = (int)Keys.RightBracket;
+        lua["unminal.keys.GraveAccent"] = (int)Keys.GraveAccent;
+        lua["unminal.keys.Escape"] = (int)Keys.Escape;
+        lua["unminal.keys.Enter"] = (int)Keys.Enter;
+        lua["unminal.keys.Tab"] = (int)Keys.Tab;
+        lua["unminal.keys.Backspace"] = (int)Keys.Backspace;
+        lua["unminal.keys.Insert"] = (int)Keys.Insert;
+        lua["unminal.keys.Delete"] = (int)Keys.Delete;
+        lua["unminal.keys.Right"] = (int)Keys.Right;
+        lua["unminal.keys.Left"] = (int)Keys.Left;
+        lua["unminal.keys.Down"] = (int)Keys.Down;
+        lua["unminal.keys.Up"] = (int)Keys.Up;
+        lua["unminal.keys.PageUp"] = (int)Keys.PageUp;
+        lua["unminal.keys.PageDown"] = (int)Keys.PageDown;
+        lua["unminal.keys.Home"] = (int)Keys.Home;
+        lua["unminal.keys.End"] = (int)Keys.End;
+        lua["unminal.keys.CapsLock"] = (int)Keys.CapsLock;
+        lua["unminal.keys.ScrollLock"] = (int)Keys.ScrollLock;
+        lua["unminal.keys.NumLock"] = (int)Keys.NumLock;
+        lua["unminal.keys.PrintScreen"] = (int)Keys.PrintScreen;
+        lua["unminal.keys.Pause"] = (int)Keys.Pause;
+        lua["unminal.keys.F1"] = (int)Keys.F1;
+        lua["unminal.keys.F2"] = (int)Keys.F2;
+        lua["unminal.keys.F3"] = (int)Keys.F3;
+        lua["unminal.keys.F4"] = (int)Keys.F4;
+        lua["unminal.keys.F5"] = (int)Keys.F5;
+        lua["unminal.keys.F6"] = (int)Keys.F6;
+        lua["unminal.keys.F7"] = (int)Keys.F7;
+        lua["unminal.keys.F8"] = (int)Keys.F8;
+        lua["unminal.keys.F9"] = (int)Keys.F9;
+        lua["unminal.keys.F10"] = (int)Keys.F10;
+        lua["unminal.keys.F11"] = (int)Keys.F11;
+        lua["unminal.keys.F12"] = (int)Keys.F12;
+        lua["unminal.keys.F13"] = (int)Keys.F13;
+        lua["unminal.keys.F14"] = (int)Keys.F14;
+        lua["unminal.keys.F15"] = (int)Keys.F15;
+        lua["unminal.keys.F16"] = (int)Keys.F16;
+        lua["unminal.keys.F17"] = (int)Keys.F17;
+        // MOUSE BUTTONS
+        lua["unminal.keys.MouseButton1"] = (int)MouseButton.Button1;
+        lua["unminal.keys.MouseButton2"] = (int)MouseButton.Button2;
+        lua["unminal.keys.MouseButton3"] = (int)MouseButton.Button3;
+        lua["unminal.keys.MouseButton4"] = (int)MouseButton.Button4;
+        lua["unminal.keys.MouseButton5"] = (int)MouseButton.Button5;
+        lua["unminal.keys.MouseButton6"] = (int)MouseButton.Button6;
+        lua["unminal.keys.MouseButton7"] = (int)MouseButton.Button7;
+        lua["unminal.keys.MouseButton8"] = (int)MouseButton.Button8;
+        lua["unminal.keys.MouseLeft"] = (int)MouseButton.Left;
+        lua["unminal.keys.MouseRight"] = (int)MouseButton.Right;
+        lua["unminal.keys.MouseMiddle"] = (int)MouseButton.Middle;
+        lua["unminal.keys.MouseLast"] = (int)MouseButton.Last;
 
         return camera;
     }   
--- src/core/Scripting/Lua/LuaWraperEngineStates.cs
+++ src/core/Scripting/Lua/LuaWraperEngineStates.cs
@@ -2,7 +2,31 @@
 // Licensed under GNU AGPLv3 with No-Misattribution Addendum
 // See LICENSE file for details.
 namespace Unminal.Core.Scripting.Lua.WraperStates;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
+[SupportedOSPlatform("windows")]
 public class WraperEngineStates {
-    
+    public bool IsKeyDown(int keyID) => Engine.CurrentKeyboard != null ? Engine.CurrentKeyboard.IsKeyDown((Keys)keyID) : false;
+    public bool IsKeyPressed(int keyID) => Engine.CurrentKeyboard != null ? Engine.CurrentKeyboard.IsKeyPressed((Keys)keyID) : false;
+    public bool IsKeyReleased(int keyID) => Engine.CurrentKeyboard != null ? Engine.CurrentKeyboard.IsKeyReleased((Keys)keyID) : false;
+    public bool IsMouseButtonDown(int buttonID) => Engine.CurrentMouse != null && Engine.CurrentMouse.IsButtonDown((MouseButton)buttonID);
+    public bool IsMouseButtonPressed(int buttonID) => Engine.CurrentMouse != null && Engine.CurrentMouse.IsButtonPressed((MouseButton)buttonID);
+    public bool IsMouseButtonReleased(int buttonID) => Engine.CurrentMouse != null && Engine.CurrentMouse.IsButtonReleased((MouseButton)buttonID);
+    public string[] args => Engine.args;
+    public float dt => Engine.DeltaTime;
+    public float tt => (float)Engine.TotalTime;
+    public bool IsDebug => Engine.IsDebug;
+    public bool InConsole => Engine.GlobalWindowState.InConsole;
+    public bool IsFullScreen => Engine.GlobalWindowState.IsFullScreen;
+    public bool InDebugMenu => Engine.GlobalWindowState.InDebugMenu;
+    public bool InPause => Engine.GlobalWindowState.InPause;
+    public string BaseFolder => Engine.Paths.BaseFolder;
+    public bool CanF3 {
+        get => Engine.CanF3;
+        set => Engine.CanF3 = value;
+    }
+    public bool ShowLight {
+        get => Engine.ShowLight;
+        set => Engine.ShowLight = value;
+    }
 }
